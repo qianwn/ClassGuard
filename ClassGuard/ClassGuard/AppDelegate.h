ec2819910d273f1aae9f2ba54faa923542832fb3
钱文 <qianwen_guichi@126.com>
2016-10-12 15:57:52 +0800

    test

---
+++ ClassGuard/ClassGuard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ClassGuard
+//
+//  Created by 钱文 on 2016/10/12.
+//  Copyright © 2016年 钱文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ClassGuard/ClassGuard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ClassGuard
+//
+//  Created by 钱文 on 2016/10/12.
+//  Copyright © 2016年 钱文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
